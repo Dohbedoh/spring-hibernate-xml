27dfb7be3b57c906344b08958d258b423428160d
Allan BURDAJEWICZ <allan.burdajewicz@gmail.com>
2015-10-23 19:53:59 +1000

    Added documentation and renamed few files.

--- src/main/java/com/dohbedoh/SpringHibernateXml.java
+++ src/main/java/com/dohbedoh/SpringHibernateXml.java
@@ -7,10 +7,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Created by Allan on 23/10/2015.
  */
-public class SpringHibernateSandbox {
+public class SpringHibernateXml {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-hibernate-sandbox.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-hibernate-xml.xml");
 
         CompanyDAO companyDAO = (CompanyDAO) context.getBean("companyDAO");
         System.out.println(companyDAO.findAll().size());
